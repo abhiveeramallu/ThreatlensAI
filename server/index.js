36af747780e95f6f6d987360dcd9d608d974e0eb
Abhi Veeramallu <abhiram97137@gmail.com>
2026-06-29 11:20:03 +0530

    Fix Render deployment

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "127.0.0.1", () => {
-  console.log(`Bot detection demo running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
